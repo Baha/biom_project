5d392c311ff7ff8fe12a249c47a8640f5cb11a2e
Baha <ukonat@gmail.com>
2013-12-16 11:55:30 +0100

    Added normalization, fixed some parameters. Added additional steps of the algorithm.

--- train_face_detector.m
+++ train_face_detector.m
@@ -1,30 +1,46 @@
 #!/usr/bin/octave -qf
 
-if (nargin != 2)
-  usage("train_face_detector <num_regions> <num_patterns>\n");
+if (nargin != 1)
+  usage("train_face_detector <num_patterns>\n");
 endif
 
 arg_list = argv();
-nRegions = str2num(arg_list{1});
-nPatterns = str2num(arg_list{2});
+%nRegions = str2num(arg_list{1});
+nRegions = 16;
+nPositions = 9;
+nPatterns = str2num(arg_list{1});
 
-#faces = dlmread("dfFaces_21x21.fmt.norm", ",");
-#no_faces = dlmread("NotFaces_21x21.fmt.norm", ",");
+faces = dlmread("dfFaces_21x21.fmt.norm", ",");
+notFaces = dlmread("NotFaces_21x21.fmt.norm", ",");
+allData = [faces; notFaces];
 
 [nFaces dimFaces] = size(faces);
-[nNotFaces ans] = size(not_faces);
+[nNotFaces ans] = size(notFaces);
+nData = nFaces + nNotFaces;
 
 V  = zeros(nRegions, nPatterns);
 nV = zeros(nRegions, nPatterns);
-% Review M matrix majors
 M  = zeros(nRegions, nPositions, nPatterns);
+
+nxRegions = nyRegions = sqrt(nRegions);
+nxPixels = nyPixels = floor(sqrt(DimFaces)) / nxRegions;
+
+regionsData = [];
+for i = 1:nData
+  for x = 1:nxRegions
+    for y = 1:nyRegions
+    curRegion = getRegionFromData(allData(i), x, y)
+    regionsData = [regionsData;
+  end
+end
 
 for i = 1:nFaces
   for j = 1:nRegions
     % Reducir dimension de la region
     for k = 1:nPositions
       % curPattern = patron que asigna kmeans
-      V(j,k,curPattern)++;
+      V(j,curPattern)++;
+      M(j,k,curPattern)++;
     end
   end
 end
@@ -32,5 +48,13 @@ end
 for i = 1:nNotFaces
   for j = 1:nRegions
     nV(j,curPattern)++;
+  end
+end
+
+for i = 1:nRegions
+  V(i,:) = V(i,:) / sum(nV(i,:);
+  nV(i,:) = nV(i,:) / sum(nV(i,:);
+  for j = 1:nPatterns
+    M(i,j,:) = M(i,j,:) / sum(M(i,j,:));
   end
 end
