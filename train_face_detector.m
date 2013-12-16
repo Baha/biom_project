2b2d6ea957fae59c4cafb7fcef83454217392471
baha <baha@eol.(none)>
2013-12-16 01:35:43 +0100

    Declared main structures of the algorithm. Better parsing of arguments.

--- train_face_detector.m
+++ train_face_detector.m
@@ -1,11 +1,36 @@
 #!/usr/bin/octave -qf
 
+if (nargin != 2)
+  usage("train_face_detector <num_regions> <num_patterns>\n");
+endif
+
+arg_list = argv();
+nRegions = str2num(arg_list{1});
+nPatterns = str2num(arg_list{2});
+
 #faces = dlmread("dfFaces_21x21.fmt.norm", ",");
 #no_faces = dlmread("NotFaces_21x21.fmt.norm", ",");
 
-if (nargin != 0)
-  printf("Usage: train_face_detector <num_regions>\n");
-endif
-  
-arg_list = argv();
-nregions = str2num(arg_list{1});
+[nFaces dimFaces] = size(faces);
+[nNotFaces ans] = size(not_faces);
+
+V  = zeros(nRegions, nPatterns);
+nV = zeros(nRegions, nPatterns);
+% Review M matrix majors
+M  = zeros(nRegions, nPositions, nPatterns);
+
+for i = 1:nFaces
+  for j = 1:nRegions
+    % Reducir dimension de la region
+    for k = 1:nPositions
+      % curPattern = patron que asigna kmeans
+      V(j,k,curPattern)++;
+    end
+  end
+end
+
+for i = 1:nNotFaces
+  for j = 1:nRegions
+    nV(j,curPattern)++;
+  end
+end
