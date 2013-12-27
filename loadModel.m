a77fe07cd26c52a22b58c6e62b02f7e571f08b12
Baha <ukonat@gmail.com>
2013-12-27 13:46:50 +0100

    Obtention of additional paramaters.

--- loadModel.m
+++ loadModel.m
@@ -1,7 +1,9 @@
-function [transVectors centroids V nV M] = loadModel()
+function [nRegions nDimensions nPatterns transVectors centroids V nV M] = loadModel()
   transVectors = dlmread("config/transVec.dat",","); 
   centroids = dlmread("config/centroids.dat",","); 
   V = dlmread("config/vecV.dat",",");
   nV = dlmread("config/vecNV.dat",",");
   M = dlmread("config/matM.dat",",");
+  [nRegions ans] = size(M);
+  [nPatterns nDimensions] = size(centroids);
 endfunction
