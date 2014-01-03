39469e2b9169a374c8752ffc29de3719fbccd058
Adrian Palacios <ukonat@gmail.com>
2014-01-03 02:56:18 +0100

    Multiscale detection is performed. pixelShift is now obtained from image size.

--- applyFaceDetector.m
+++ applyFaceDetector.m
@@ -1,18 +1,16 @@
 function applyFaceDetector(imageFileName, threshold)
     windowLength = 21;
-    pixelShift = [4];
     scale = 1.0;
     
     originalImage = imread(imageFileName);
     grayscaleImage = double(rgb2gray(originalImage));
     taggedImage = originalImage;
-
     [nRegions, nPatterns, transVectors, centroids, V, nV, M] = loadModel();
-
-    for s = 1:1
-        [normImage, maxX, maxY] = scaleImage(grayscaleImage, scale);
-        for j = 1:pixelShift(s):maxY-windowLength
-          for i = 1:pixelShift(s):maxX-windowLength
+    
+    for s = 1:5
+        [normImage, pixelShift, maxX, maxY] = scaleImage(grayscaleImage, scale);
+        for j = 1:pixelShift:maxY-windowLength
+          for i = 1:pixelShift:maxX-windowLength
             imageWindow = normImage(i:(i+windowLength-1),j:(j+windowLength-1));
             score = computeScore(imageWindow,nRegions,nPatterns,windowLength,transVectors,centroids,V,nV,M);
             if (score > threshold)
@@ -21,7 +19,7 @@ function applyFaceDetector(imageFileName, threshold)
             end
           end
         end
-        imwrite(taggedImage, 'tagged.jpg', 'jpg');
         scale = scale * 0.9;
     end
+    imwrite(taggedImage, 'tagged.jpg', 'jpg');
 end
--- scaleImage.m
+++ scaleImage.m
@@ -1,10 +1,24 @@
-function [normImage, maxX, maxY] = scaleImage(image,scaleFactor)
-if (scaleFactor <= 1.0)
-    image = imresize(image, scaleFactor);
-end
-    [maxX, maxY] = size(image);
-    imageMean = mean2(image);
-    imageStd = std2(image);
-    normImage = (image - imageMean) / imageStd;
+function [normImage, pixelShift, maxX, maxY] = scaleImage(image,scaleFactor)
+    if (scaleFactor <= 1.0)
+        image = imresize(image, scaleFactor);
+    end
+        [maxX, maxY] = size(image);
+        imageMean = mean2(image);
+        imageStd = std2(image);
+        normImage = (image - imageMean) / imageStd;
+        if (maxY > 1024)
+            pixelShift = 32;
+        else if (maxY > 512)
+            pixelShift = 16;
+            else if (maxY > 256)
+                    pixelShift = 8;
+                else if (maxY > 128)
+                        pixelShift = 4;
+                    else
+                        pixelShift = 2;
+                    end
+                end
+            end
+        end
 end
 
