78c9ac8d148b0bbb37767815994b4ae190e60f3f
Adrian Palacios <ukonat@gmail.com>
2014-01-20 22:05:11 +0100

    Checks are made before deleting any folder.

--- createDataFiles.m
+++ createDataFiles.m
@@ -1,6 +1,8 @@
 function createDataFiles(transVectors,centroids,V,nV,M)
-  rmdir('config','s');
-  mkdir('config');
+  if (exist('config','dir'))
+      rmdir('config','s');
+  end
+    mkdir('config');
   dlmwrite('config/transVec.dat',transVectors,','); 
   dlmwrite('config/centroids.dat',centroids,','); 
   dlmwrite('config/vecV.dat',V,',');
